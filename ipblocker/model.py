c206f7706f6f27bd195c858aa3187dc13fe140cb
Justin Azoff <JAzoff@albany.edu>
2008-05-16 18:36:38 +0000

    new project
    git-svn-id: svn+ssh://svn/home/svnrepos/main/code/trunk/ipblocker@4552 b0e434f6-cefe-0310-9fc7-ee6d912223e3

---
+++ ipblocker/model.py
@@ -0,0 +1,80 @@
+from sqlalchemy import *
+from sqlalchemy.orm import *
+from sqlalchemy.exceptions import SQLError
+from sqlalchemy import *
+import sqlalchemy.types as sqltypes
+import ConfigParser
+
+import datetime
+
+c = ConfigParser.ConfigParser()
+c.read(['/etc/ipblocker/db.cfg','db.cfg'])
+engine = create_engine(c.get('db','uri'))
+Session = scoped_session(sessionmaker(autoflush=True, transactional=False, bind=engine))
+metadata = MetaData(bind=engine)
+mapper = Session.mapper
+
+class PGMac(sqltypes.TypeEngine):
+    def get_col_spec(self):
+        return "MACADDR"
+
+class PGInet(sqltypes.TypeEngine):
+    def get_col_spec(self):
+        return "INET"
+
+
+blocks = Table('blocks', metadata,
+    Column('id',        Integer, primary_key=True),
+    Column('ip',        PGInet, index=True),
+    Column('who',       String(50), nullable=False),
+    Column('comment',   String, nullable=False),
+    Column('entered',   DateTime, default=datetime.datetime.now),
+    Column('blocked',   DateTime, index=True),
+    Column('unblocked', DateTime, index=True),
+    Column('unblock_at', DateTime, nullable=False),
+    Column('unblock_now',   Boolean, default=False,nullable=False)
+)
+
+
+class Block(object):
+
+    def __repr__(self):
+        return 'Block(ip="%s")' % self.ip
+
+    @classmethod
+    def get_blocked(cls):
+        return cls.query.filter(and_(cls.blocked!=None,cls.unblocked==None)).all()
+
+    @classmethod
+    def get_block_pending(cls):
+        return cls.query.filter(cls.blocked==None).all()
+
+    @classmethod
+    def get_unblock_pending(cls):
+        return cls.query.filter(and_(
+                or_(cls.unblock_now==True, func.now() > cls.unblock_at),
+                cls.unblocked==None)).all()
+
+    def set_blocked(self):
+        self.blocked = datetime.datetime.now()
+        Session.flush()
+
+    def set_unblocked(self):
+        self.unblocked = datetime.datetime.now()
+        Session.flush()
+
+    def set_unblock_now(self):
+        self.unblock_now = True
+        Session.flush()
+    
+
+def block_ip(ip, who, comment, duration):
+    now = datetime.datetime.now()
+    diff = datetime.timedelta(seconds=duration)
+    unblock_at = now + diff
+    b = Block(ip=ip, who=who, comment=comment, unblock_at=unblock_at)
+    Session.flush()
+    return b
+
+mapper(Block, blocks)
+

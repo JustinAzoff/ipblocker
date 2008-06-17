b4d09e0154c1573520f729e09e3154580196ab82
Justin Azoff <JAzoff@albany.edu>
2008-06-17 20:36:14 +0000

    use a custom groupby func that doesn't require a sorted list move more common functions into util
    git-svn-id: svn+ssh://svn/home/svnrepos/main/code/trunk/ipblocker@4703 b0e434f6-cefe-0310-9fc7-ee6d912223e3

---
+++ ipblocker/util.py
@@ -0,0 +1,11 @@
+def groupby(it, keyfunc):
+    """Like itertools.groupby, but don't require a sorted list"""
+    ret = {}
+    for x in it:
+        key = keyfunc(x)
+        ret.setdefault(key, []).append(x)
+    return ret
+
+def subnet(ip):
+    """1.2.3.4 -> 3"""
+    return (ip.int()/256)&255

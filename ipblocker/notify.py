03bc75a6f7d1e84ad10d106c978dacdf6c511640
Justin Azoff <JAzoff@albany.edu>
2008-07-14 18:36:56 +0000

    make this more robust
    git-svn-id: svn+ssh://svn/home/svnrepos/main/code/trunk/ipblocker@4779 b0e434f6-cefe-0310-9fc7-ee6d912223e3

--- ipblocker/notify.py
+++ ipblocker/notify.py
@@ -1,8 +1,11 @@
 from amqpevent import Events
-e=Events('notify')
+e = None
 
 def notify_block(b):
+    global e
     try :
+        if e is None:
+            e=Events('notify')
         e.add_event("ipb: %s\n%s" % ( b.ip, b.comment))
     except:
         pass

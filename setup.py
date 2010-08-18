8d405326481390b0ae68fa1a6059dcf17c34e8d3
Justin Azoff <JAzoff@albany.edu>
2010-08-18 18:42:39 +0000

    re-indent
    git-svn-id: svn+ssh://svn/home/svnrepos/main/code/trunk/ipblocker@6328 b0e434f6-cefe-0310-9fc7-ee6d912223e3

--- setup.py
+++ setup.py
@@ -5,31 +5,31 @@ from glob import glob
 version = '0.5'
 
 setup(name='ipblocker',
-      version=version,
-      description="IP Blocker",
-      long_description="""\
+    version=version,
+    description="IP Blocker",
+    long_description="""\
 """,
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      keywords='',
-      author='Justin Azoff',
-      author_email='JAzoff@uamail.albany.edu',
-      url='',
-      license='MIT',
-      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          "SQLAlchemy >= 0.4",
-          "WebHelpers",
-          "cisco",
-          "tcpsleep",
-          "pysubnettree",
-          # -*- Extra requirements: -*-
-      ],
-      entry_points="""
-      [paste.app_install]
-      main = paste.script.appinstall:Installer
-      """,
-      test_suite='nose.collector',
-      scripts=glob('scripts/*'),
-      )
+    classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    keywords='',
+    author='Justin Azoff',
+    author_email='JAzoff@uamail.albany.edu',
+    url='',
+    license='MIT',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        "SQLAlchemy >= 0.4",
+        "WebHelpers",
+        "cisco",
+        "tcpsleep",
+        "pysubnettree",
+        # -*- Extra requirements: -*-
+    ],
+    entry_points="""
+    [paste.app_install]
+    main = paste.script.appinstall:Installer
+    """,
+    test_suite='nose.collector',
+    scripts=glob('scripts/*'),
+    )

db8142bde2390664c308d4419168b40affaee19a
Justin Azoff <JAzoff@albany.edu>
2012-03-15 18:26:29 +0000

    make the snort blocker work on multiple files at once
    git-svn-id: svn+ssh://svn/home/svnrepos/main/code/trunk/ipblocker@8013 b0e434f6-cefe-0310-9fc7-ee6d912223e3

--- ipblocker/tail.py
+++ ipblocker/tail.py
@@ -21,3 +21,31 @@ def tail(fn):
             if time.time() - last > 120 and getsize() < size:
                 f.close()
                 f = open(fn)
+
+def stat_size(fn):
+    return os.stat(fn).st_size
+
+def multitail(filenames):
+    files = {}
+    for fn in filenames:
+        handle = open(fn)
+        handle.seek(0, 2)
+        files[fn] = dict(handle=handle, last=0,size=0)
+
+    all = files.items()
+    while 1:
+        read = False
+        for fn, info in all:
+            l = info['handle'].readline()
+            if l:
+                yield fn, l
+                info['last'] = time.time()
+                info['size'] = stat_size(fn)
+                read = True
+        if not read:
+            time.sleep(1)
+        for fn, info in all:
+            if time.time() - info['last'] > 20 and stat_size(fn) < info['size']:
+                info['handle'].close()
+                info['handle'] = open(fn)
+            

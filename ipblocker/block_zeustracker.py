1a3d44f1d5d212cc670d495912108239a5f01368
Justin Azoff <JAzoff@albany.edu>
2012-05-24 17:25:39 +0000

    fix ssl issue
    git-svn-id: svn+ssh://svn/home/svnrepos/main/code/trunk/ipblocker@8435 b0e434f6-cefe-0310-9fc7-ee6d912223e3

--- ipblocker/block_zeustracker.py
+++ ipblocker/block_zeustracker.py
@@ -16,7 +16,7 @@ class ZeusBlocker(SourceBlocker):
 
 
     def get_records(self):
-        h = httplib2.Http(os.path.expanduser("~/.httplib2_cache"))
+        h = httplib2.Http(cache=os.path.expanduser("~/.httplib2_cache"), ca_certs='/etc/ssl/certs/ca-certificates.crt')
         logger.debug("Fetching IP list from the zeus tracker")
         resp, content = h.request("https://zeustracker.abuse.ch/blocklist.php?download=ipblocklist")
         ips = content.split("\n\n")[1].split()

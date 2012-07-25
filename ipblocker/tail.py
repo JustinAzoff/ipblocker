782de9fa3f72ac9b29718f7be422ac29f334d2d0
Justin Azoff <JAzoff@albany.edu>
2012-07-25 14:21:20 +0000

    fix tail handling of rotated files
    git-svn-id: svn+ssh://svn/home/svnrepos/main/code/trunk/ipblocker@8637 b0e434f6-cefe-0310-9fc7-ee6d912223e3

--- ipblocker/tail.py
+++ ipblocker/tail.py
@@ -1,51 +1,80 @@
 #!/usr/bin/env python
 import os
 import time
+
+def open_wait(fn):
+    while True:
+        try:
+            f = open(fn)
+            return f
+        except IOError:
+            time.sleep(1)
+
+
 def tail(fn):
     """Like tail -f """
     last = time.time()
 
-    getsize = lambda: os.stat(fn).st_size
-    size = getsize()
-    f = open(fn)
+    inode = stat_inode(fn)
+    f = open_wait(fn)
     f.seek(0, 2)
+    changed = False
 
     while 1:
         l = f.readline()
         if l:
             yield l
             last = time.time()
-            size = getsize()
         else:
-            time.sleep(1)
-            if time.time() - last > 120 and getsize() < size:
+            if changed:
                 f.close()
-                f = open(fn)
+                f = open_wait(fn)
+                inode = os.fstat(f.fileno()).st_ino
+                changed = False
+            if stat_inode(fn) != inode:
+                #set changed to true, but then try reading the file more to
+                #check to see if it was written to after rotated
+                changed = True
+            else:
+                time.sleep(1)
 
-def stat_size(fn):
-    return os.stat(fn).st_size
+def stat_inode(fn):
+    try :
+        return os.stat(fn).st_ino
+    except OSError:
+        return None
 
 def multitail(filenames):
     files = {}
     for fn in filenames:
-        handle = open(fn)
-        handle.seek(0, 2)
-        files[fn] = dict(handle=handle, last=0,size=0)
+        try :
+            handle = open(fn)
+            handle.seek(0, 2)
+            inode = os.fstat(handle.fileno()).st_ino
+        except IOError:
+            handle = None
+            inode = None
+        files[fn] = dict(handle=handle, last=0,inode=inode)
 
     all = files.items()
     while 1:
         read = False
         for fn, info in all:
-            l = info['handle'].readline()
+            l = info['handle'] and info['handle'].readline()
             if l:
                 yield fn, l
                 info['last'] = time.time()
-                info['size'] = stat_size(fn)
-                read = True
+                info['read'] = read = True
+            else:
+                info['read'] = False
+        for fn, info in all:
+            if not info['handle'] or (info['read'] == False and info['inode'] != stat_inode(fn)):
+                if info['handle']:
+                    info['handle'].close()
+                try :
+                    info['handle'] = open(fn)
+                    info['inode'] = os.fstat(info['handle'].fileno()).st_ino
+                except IOError:
+                    info['handle'] = None
         if not read:
             time.sleep(1)
-        for fn, info in all:
-            if time.time() - info['last'] > 20 and stat_size(fn) < info['size']:
-                info['handle'].close()
-                info['handle'] = open(fn)
-            
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import sys, os
 from glob import glob
 
-version = '0.7.2'
+version = '0.7.3'
 
 setup(name='ipblocker',
     version=version,

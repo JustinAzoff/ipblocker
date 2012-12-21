7e2e78ed18456f0124ade2f5354d0c54473efb70
Justin Azoff <JAzoff@albany.edu>
2012-12-21 16:31:40 +0000

    fix to use env variable too
    git-svn-id: svn+ssh://svn/home/svnrepos/main/code/trunk/ipblocker@9359 b0e434f6-cefe-0310-9fc7-ee6d912223e3

--- ipblocker/logger.py
+++ ipblocker/logger.py
@@ -1,7 +1,11 @@
 import logging
 import logging.config
 
-logging.config.fileConfig(["/etc/ipblocker/ipblocker.cfg", "ipblocker.cfg"])
+env = os.getenv("IPBLOCKER_CONFIG")
+if env:
+    logging.config.fileConfig([env])
+else:
+    logging.config.fileConfig(["/etc/ipblocker/ipblocker.cfg", "ipblocker.cfg"])
 
 
 #create logger
